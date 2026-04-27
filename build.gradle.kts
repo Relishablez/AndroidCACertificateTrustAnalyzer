0104c7745fb775fda8ccbdea0fe326a4dba68d2e
Relishablez <61218149+Relishablez@users.noreply.github.com>
2026-04-27 23:32:08 +0100

    Remove allprojects block from build.gradle.kts
    Removed allprojects block and its repositories configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.kapt") version "1.9.20" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
